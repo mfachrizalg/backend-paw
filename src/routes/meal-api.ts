0342c7de09c8cf8842684979756b11e8311e3ec0
mfachrizalg <mohammad.fachrizal123@gmail.com>
2024-11-26 20:34:46 +0700

    fix: routing

--- src/routes/meal-api.ts
+++ src/routes/meal-api.ts
@@ -5,6 +5,6 @@ import { userMiddleware } from "../middlewares/user-middleware"
 export const mealRouter = express.Router()
 mealRouter.get('/api/meal/', MealController.getMeal);
 mealRouter.post('/api/meal/schedule', userMiddleware, MealController.addMealToSchedule);
-mealRouter.post('/api/meal/bookmar/:mealDBid', userMiddleware, MealController.bookmarkMeal);
+mealRouter.post('/api/meal/bookmark/:mealDBid', userMiddleware, MealController.bookmarkMeal);
 mealRouter.get('/api/meal/bookmark', userMiddleware, MealController.getBookmarkedMeals);
 mealRouter.get('/api/meal/schedule', userMiddleware, MealController.getScheduleMeals);
