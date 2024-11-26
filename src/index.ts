881aca73373a8ac5a6b9c73c8c67d43dad3ff3a1
mfachrizalg <mohammad.fachrizal123@gmail.com>
2024-11-26 17:39:09 +0700

    try fix for vercel deploy

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { mealRouter } from './routes/meal-api';
 import cors from 'cors';
 
 const app = express();
-app.use(cors());
+app.use(cors({credentials: true, origin: true}));
 app.use(express.json());
 app.use(userRouter);
 app.use(mealRouter);
