afa27076091de47cdf8d68c39f798bccbe6748af
mfachrizalg <mohammad.fachrizal123@gmail.com>
2024-11-26 16:39:14 +0700

    try fix for vercel deploy

--- src/middlewares/error-middleware.ts
+++ src/middlewares/error-middleware.ts
@@ -1,9 +1,13 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { ResponseError } from "../error/response-error";
 import { ZodError } from "zod";
 
-export const errorMiddleware = async(error: Error, req: Request, res: Response) => {
-    if (error instanceof ResponseError) res.status(error.statusCode).json({ errors: error.message });
-    else if (error instanceof ZodError) res.status(400).json({ errors: `Validation Error: ${JSON.stringify(error)}` });
-    else res.status(500).json({ errors: "Internal Server Error" });
+export const errorMiddleware = (error: Error, req: Request, res: Response, next: NextFunction) => {
+    if (error instanceof ResponseError) {
+        res.status(error.statusCode).json({ errors: error.message });
+    } else if (error instanceof ZodError) {
+        res.status(400).json({ errors: `Validation Error: ${JSON.stringify(error)}` });
+    } else {
+        res.status(500).json({ errors: "Internal Server Error" });
+    }
 }
