1c9acb35350420df1415890457e9287b33232150
mfachrizalg <mohammad.fachrizal123@gmail.com>
2024-11-28 14:54:59 +0700

    try fix deploy

--- src/routes/meal-api.ts
+++ src/routes/meal-api.ts
@@ -1,10 +1,10 @@
-import express from "express"
-import { MealController } from "../controllers/meal-controller"
-import { userMiddleware } from "../middlewares/user-middleware"
+import express from 'express';
+import { MealController } from "../controllers/meal-controller";
+import { userMiddleware } from "../middlewares/user-middleware";
 
-export const mealRouter = express.Router()
-mealRouter.get('/api/meal', MealController.getMeal);
+export const mealRouter = express.Router();
 mealRouter.get('/api/meal/bookmark', userMiddleware, MealController.getBookmarkedMeals);
+mealRouter.get('/api/meal', MealController.getMeal);
 mealRouter.post('/api/meal/schedule', userMiddleware, MealController.addMealToSchedule);
 mealRouter.get('/api/meal/schedule', userMiddleware, MealController.getScheduleMeals);
 mealRouter.post('/api/meal/bookmark/:mealDBid', userMiddleware, MealController.bookmarkMeal);
